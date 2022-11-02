5b88d230bc9150e7fdb0def058e6dc8e95cadd80
Ndueso Walter <69187327+walterjnr1@users.noreply.github.com>
2022-11-02 12:32:59 +0100

    Update transaction.dart

--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -40,7 +40,9 @@ class Transaction {
     'paymentID': paymentID,    'customer_email': customer_email,    'restuarant': restuarant,   'amount': amount,  'date_payment': date_payment,  };
 }
 class Env {
-  static String URL_PREFIX = "http://192.168.43.16/Qr_code_payment_system/restuarant";
-//static String URL_PREFIX = "https://qrcode.leastpayproject.com.ng/restuarant";
+  //static String URL_PREFIX = "http://192.168.43.16/Qr_code_payment_system/restuarant";
+static String URL_PREFIX = "https://qrcode.leastpayproject.com.ng/restuarant";
+  //static String URL_PREFIX = "https://qrcode.americanlandbank.org/restuarant";
+
 
 }
